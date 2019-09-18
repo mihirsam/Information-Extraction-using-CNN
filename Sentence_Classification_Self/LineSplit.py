09e8d8b93f17f22c0aecc00a80aa3291b52c12da
Mihir <xenon.of.x@gmail.com>
2019-09-18 06:53:59 +0530

    starts main implementation. Implements line_split with doc2vec

--- Sentence_Classification_Self/LineSplit.py
+++ Sentence_Classification_Self/LineSplit.py
@@ -1,32 +1,32 @@
 import re
 
-# reading text file
-data = open('worldwar.txt', encoding='utf-8', mode='r')
-data_lines = data.readlines()
+def LineSplit(filename):
+    # reading text file
+    data = open(filename, encoding='utf-8', mode='r')
+    data_lines = data.readlines()
 
-# replacing U.S. with US and spliting lines
-cleaned_data = []
+    # replacing U.S. with US and spliting lines
+    cleaned_data = []
 
-for line in data_lines:
-        cleaned_data.append(line.replace('U.S.', 'US'))
-        
-lis = []
+    for line in data_lines:
+            cleaned_data.append(line.replace('U.S.', 'US'))
 
-for line in cleaned_data:
-    if len(set(line)) > 1:
-        lis += line.split('.')
+    lis = []
 
-# removing unwanted elements and putting cleaned line in cleaned_data
-cleaned_data = []
-for line in lis:
-    if len(line) >= 1:
-        if line[0] == " ":
-            line = line[1:]
-        line = re.sub(r'\[.*?\]', "", line)
-        
-        if line != '\n':
-            cleaned_data.append(line)
+    for line in cleaned_data:
+        if len(set(line)) > 1:
+            lis += line.split('.')
 
-# printing each line of cleaned_data
-for line in cleaned_data:
-    print(line, "\n")
+    # removing unwanted elements and putting cleaned line in cleaned_data
+    cleaned_data = []
+    for line in lis:
+        if len(line) >= 1:
+            if line[0] == " ":
+                line = line[1:]
+            line = re.sub(r'\[.*?\]', "", line)
+
+            if line != '\n':
+                cleaned_data.append(line)
+
+    # printing each line of cleaned_data
+    return cleaned_data
