90575e756e53b79a43d2a178adb6b35d61e7a07a
Mihir <xenon.of.x@gmail.com>
2019-10-17 03:28:55 +0530

    fix cnn file according to input files

--- Sentence_Classification_Self/CNN.py
+++ Sentence_Classification_Self/CNN.py
@@ -1,45 +1,33 @@
-# imports
-import os
-import sys
-import numpy as np
-import keras
-from keras.preprocessing.text import Tokenizer
-from keras.utils import to_categorical
-from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Activation, Conv2D, Input, Embedding, Reshape, MaxPool2D, Concatenate, Flatten, Dropout, Dense, Conv1D
-from keras.layers import MaxPool1D
-from keras.models import Model
-from keras.callbacks import ModelCheckpoint
-from keras.optimizers import Adam
-
 def tCNN():
-    # convolution neural network
+    
+    # imports
+    import os
+    import sys
+    import numpy as np
+    import keras
+    from keras.preprocessing.text import Tokenizer
+    from keras.utils import to_categorical, plot_model
+    from keras.preprocessing.sequence import pad_sequences
+    from keras.layers import Activation, Conv2D, Input, Embedding, Reshape, MaxPool2D, Concatenate, Flatten, Dropout, Dense, Conv1D
+    from keras.layers import MaxPool1D
+    from keras.models import Model, Sequential
+    from keras.callbacks import ModelCheckpoint
+    from keras.optimizers import Adam
+        
+ 
 
-    inputs = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
-    embedding = embedding_layer(inputs)
+    model = Sequential()
+    
+    model.add(Conv2D(128, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu', input_shape = (10, 10, 3)))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(1,1), padding='valid'))
+    model.add(Conv2D(64, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(1,1), padding='valid'))
+    model.add(Conv2D(32, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu'))
+    model.add(MaxPool2D(pool_size=(2, 2), strides=(1,1), padding='valid'))
+    
+    model.add(Flatten())
+    model.add(Dense(units=8, activation='softmax'))
 
-    print(embedding.shape)
-    reshape = Reshape((MAX_SEQUENCE_LENGTH,EMBEDDING_DIM,1))(embedding)
-    print(reshape.shape)
-
-    conv_0 = Conv2D(num_filters, kernel_size=(filter_sizes[0], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
-    conv_1 = Conv2D(num_filters, kernel_size=(filter_sizes[1], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
-    conv_2 = Conv2D(num_filters, kernel_size=(filter_sizes[2], embedding_dim), padding='valid', kernel_initializer='normal', activation='relu')(reshape)
-
-    maxpool_0 = MaxPool2D(pool_size=(MAX_SEQUENCE_LENGTH - filter_sizes[0] + 1, 1), strides=(1,1), padding='valid')(conv_0)
-    maxpool_1 = MaxPool2D(pool_size=(MAX_SEQUENCE_LENGTH - filter_sizes[1] + 1, 1), strides=(1,1), padding='valid')(conv_1)
-    maxpool_2 = MaxPool2D(pool_size=(MAX_SEQUENCE_LENGTH - filter_sizes[2] + 1, 1), strides=(1,1), padding='valid')(conv_2)
-
-    concatenated_tensor = Concatenate(axis=1)([maxpool_0, maxpool_1, maxpool_2])
-    flatten = Flatten()(concatenated_tensor)
-    dropout = Dropout(drop)(flatten)
-    output = Dense(units=20, activation='softmax')(dropout)
-
-    # this creates a model that includes
-    model = Model(inputs=inputs, outputs=output)
-
-    checkpoint = ModelCheckpoint('weights_cnn_sentece.hdf5', monitor='val_acc', verbose=1, save_best_only=True, mode='auto')
     adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-
     model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
     model.summary()
