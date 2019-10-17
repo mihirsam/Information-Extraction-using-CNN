50ef74b98279f94f96bf02d5f69521a2c2ed428a
Mihir <xenon.of.x@gmail.com>
2019-10-17 22:59:43 +0530

    fix dense layer problem

--- Sentence_Classification_Self/CNN.py
+++ Sentence_Classification_Self/CNN.py
@@ -13,21 +13,30 @@ def tCNN():
     from keras.models import Model, Sequential
     from keras.callbacks import ModelCheckpoint
     from keras.optimizers import Adam
-        
+    from keras.models import load_model    
  
-
-    model = Sequential()
+    visible = Input(shape=(10, 10, 1))
     
-    model.add(Conv2D(128, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu', input_shape = (10, 10, 3)))
-    model.add(MaxPool2D(pool_size=(2, 2), strides=(1,1), padding='valid'))
-    model.add(Conv2D(64, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu'))
-    model.add(MaxPool2D(pool_size=(2, 2), strides=(1,1), padding='valid'))
-    model.add(Conv2D(32, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu'))
-    model.add(MaxPool2D(pool_size=(2, 2), strides=(1,1), padding='valid'))
+    conv1 = Conv2D(64, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu')(visible)
+    conv2 = Conv2D(64, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu')(visible)
+    conv3 = Conv2D(64, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu')(visible)
     
-    model.add(Flatten())
-    model.add(Dense(units=8, activation='softmax'))
+    pool1 = MaxPool2D(pool_size=(2, 2), strides=(1,1), padding='valid')(conv1)
+    pool2 = MaxPool2D(pool_size=(2, 2), strides=(1,1), padding='valid')(conv2)
+    pool3 = MaxPool2D(pool_size=(2, 2), strides=(1,1), padding='valid')(conv3)
+    
+    concatenated_tensor = Concatenate(axis=1)([pool1, pool2, pool3])
+    flatten = Flatten()(concatenated_tensor)
+    output = Dense(units=8, activation='softmax')(flatten)
+    
+    model = Model(inputs=visible, outputs=output)
 
     adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-    model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer=adam, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     model.summary()
+    
+    try:
+        model.save('CNN_model_init.h5')
+        print("Model Saved!")
+    except:
+        print("Error in saving Model")
