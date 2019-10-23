f99cdc518e40d6eb6471cb1e134e322895ad4673
Mihir <xenon.of.x@gmail.com>
2019-10-23 15:01:21 +0530

    add padding to existing layers

--- Sentence_Classification_Self/CNN.py
+++ Sentence_Classification_Self/CNN.py
@@ -8,7 +8,7 @@ def tCNN():
     from keras.preprocessing.text import Tokenizer
     from keras.utils import to_categorical, plot_model
     from keras.preprocessing.sequence import pad_sequences
-    from keras.layers import Activation, Conv2D, Input, Embedding, Reshape, MaxPool2D, Concatenate, Flatten, Dropout, Dense, Conv1D
+    from keras.layers import Activation, Conv2D, Input, Embedding, Reshape, MaxPool2D, Concatenate, Flatten, Dropout, Dense, Conv1D, ZeroPadding2D
     from keras.layers import MaxPool1D
     from keras.models import Model, Sequential
     from keras.callbacks import ModelCheckpoint
@@ -17,9 +17,13 @@ def tCNN():
  
     visible = Input(shape=(10, 10, 1))
     
-    conv1 = Conv2D(64, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu')(visible)
-    conv2 = Conv2D(64, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu')(visible)
-    conv3 = Conv2D(64, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu')(visible)
+    padd1 = ZeroPadding2D(2)(visible)
+    padd2 = ZeroPadding2D(2)(visible)
+    padd3 = ZeroPadding2D(2)(visible)
+    
+    conv1 = Conv2D(64, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu')(padd1)
+    conv2 = Conv2D(64, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu')(padd2)
+    conv3 = Conv2D(64, kernel_size=(3, 3), padding='valid', kernel_initializer='normal', activation='relu')(padd3)
     
     pool1 = MaxPool2D(pool_size=(2, 2), strides=(1,1), padding='valid')(conv1)
     pool2 = MaxPool2D(pool_size=(2, 2), strides=(1,1), padding='valid')(conv2)
